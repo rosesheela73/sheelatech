8752447ea17f3c4df883b4e45bd1cb142e64cecc
rosesheela73 <rosesheela73@gmail.com>
2023-09-24 18:33:00 +0530

    Initial code abase

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+names=("sheela","sharon","aruna","6","sharon")
+namesSet={"sheela","sharon","aruna","6","sharon"}
+for i in namesSet:
+    print(i)
+
+rolcall={1:"sheela",2:"thiru",3:"aruna",4:"sharon"}
+for i in rolcall.items():
+    print(i)
+
+for i in rolcall.keys():
+    print(i)
+
+for i in rolcall.values():
+    print(i)
+
+# rolcall[3]="attaiah"
+# print(rolcall.get(3))
+
+# for i in rolcall.items():
+#     print(i)
+
